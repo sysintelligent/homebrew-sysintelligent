c0cb7bc97e3f33c61c58e6a739dc9e5be6cf4eb0
David Gu <d.gu@sap.com>
2025-04-23 15:27:02 -0700

    chore: update bdc-cli to v1.0.2

--- Formula/bdc-cli.rb
+++ Formula/bdc-cli.rb
@@ -1,14 +1,36 @@
 class BdcCli < Formula
   desc "A tool between developers and complex backend infrastructure"
   homepage "https://github.com/sysintelligent/bdc-bridge"
-  url "https://github.com/sysintelligent/bdc-bridge/archive/v1.0.1.tar.gz"
-  sha256 "0a39ab5a0cbdf83c9a2744575e53174a1f6b5bfcf571e05b3ede2d11692831d4"
+  url "https://github.com/sysintelligent/bdc-bridge/archive/v1.0.2.tar.gz"
+  sha256 "7269fbd7148dfd5daef88365122662162018548472776cd2d348b63397a8d3ab"
 
   depends_on "go" => :build
+  depends_on "node" => :build
+  depends_on "npm" => :build
 
   def install
     system "go", "build", "-o", "bdc-cli", "./cmd/bdc-cli"
     bin.install "bdc-cli"
+
+    # Install UI files
+    cd "ui" do
+      system "npm", "install"
+      system "npm", "run", "build"
+      
+      # Create the share directory
+      share_dir = share/"bdc-cli"
+      share_dir.mkpath
+      
+      # Install UI files
+      ui_dir = share_dir/"ui"
+      ui_dir.mkpath
+      
+      # Copy all files from the build directory
+      system "cp", "-R", ".next", ui_dir
+      system "cp", "-R", "public", ui_dir
+      system "cp", "package.json", ui_dir
+      system "cp", "next.config.js", ui_dir
+    end
   end
 
   test do
