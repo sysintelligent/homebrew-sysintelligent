c7bfe76f7e35fb6ea1098fff30e90c583f70f63f
David Gu <d.gu@sap.com>
2025-04-25 14:28:31 -0700

    Update bdc-cli to version 1.0.4

--- Formula/bdc-cli.rb
+++ Formula/bdc-cli.rb
@@ -2,7 +2,7 @@ class BdcCli < Formula
   desc "A tool between developers and complex backend infrastructure"
   homepage "https://github.com/sysintelligent/bdc-bridge"
   url "https://github.com/sysintelligent/bdc-bridge/archive/v1.0.4.tar.gz"
-  sha256 "d9a1697aa4da768046e86a37263b4ee128a0b1939db57992be324088709e2ac9"
+  sha256 "098a72cb40af0e83f8613cd4627293d4be73dc98789640acaa220ce82c3e19f7"
 
   depends_on "go" => :build
   depends_on "node" => :build
